a136bb22ed065016885865f84c83b2cc4b3732c6
Jamie Bliss <jamie@ivyleav.es>
2019-12-27 21:43:44 -0500

    Rewrite for `localmc` lib

---
+++ localmc/src/lib.rs
@@ -0,0 +1,80 @@
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
+
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::env;
+
+#[macro_use] extern crate failure;
+
+use failure::Fallible;
+
+enum PropLine {
+    Comment,
+    Prop(String, String)
+}
+
+#[derive(Debug, Fail)]
+pub enum PropParseError {
+    #[fail(display = "No = found in line")]
+    NoValue
+}
+
+impl FromStr for PropLine {
+    type Err = PropParseError;
+
+    fn from_str(txt: &str) -> Result<Self, Self::Err> {
+        if txt.starts_with("#") {
+            return Ok(PropLine::Comment);
+        }
+        let bits: Vec<&str> = txt.splitn(2, '=').collect();
+        if bits.len() != 2 {
+            return Err(PropParseError::NoValue);
+        }
+        return Ok(PropLine::Prop(bits[0].to_string(), bits[1].to_string()))
+    }
+}
+
+pub fn read_properties(path: &Path) -> Fallible<HashMap<String, String>>  {
+    let mut props = HashMap::new();
+    for line in io::BufReader::new(File::open(path)?).lines() {
+        match line?.parse::<PropLine>()? {
+            PropLine::Comment => {},
+            PropLine::Prop(k, v) => {props.insert(k, v);}
+        }
+    }
+    Ok(props)
+}
+
+
+pub fn find_root() -> Option<PathBuf> {
+    // 1. Environment variable MINECRAFT_ROOT
+    for (key, value) in env::vars() {
+        if key == "MINECRAFT_ROOT" {
+            return Some(PathBuf::from(value));
+        }
+    }
+    // 2. The container's predefined root
+    let container_root = Path::new("/mc"); // TODO: Can we read this from a compile flag?
+    if container_root.exists() {
+        return Some(container_root.to_path_buf());
+    }
+    // Can't find it
+    None
+}
+
+pub fn find_serverprops() -> Option<PathBuf> {
+    match find_root() {
+        Some(p) => Some(p.join("server.properties")),
+        None => None
+    }
+}
--- rcon/src/main.rs
+++ rcon/src/main.rs
@@ -1,10 +1,8 @@
-use std::path::PathBuf;
-use std::str::FromStr;
-use std::fs::File;
-use std::io::{self, BufRead};
 use std::path::Path;
 use std::process::exit;
 use std::env;
+use failure::Error;
+use localmc::{read_properties, find_serverprops};
 
 extern crate rcon;
 
@@ -17,7 +15,7 @@ fn main() {
     }
     let cmd = &args[1];
 
-    match read_properties(&match find_serverprops() {
+    match read_portauth(&match find_serverprops() {
         Some(p) => p,
         None => {
             eprintln!("Unable to find server.properties");
@@ -48,77 +46,10 @@ fn run_cmd(addr: &str, auth: &str, cmd: &str) -> rcon::Result<String> {
     conn.cmd(cmd)
 }
 
-enum PropLine {
-    Comment,
-    Prop(String, String)
-}
-
-enum PropParseError {
-    NoValue
-}
-
-impl Into<io::Error> for PropParseError {
-    fn into(self) -> io::Error {
-        match self {
-            PropParseError::NoValue => io::Error::new(io::ErrorKind::InvalidData, "No = found in line")
-        }
-    }
-}
-
-impl FromStr for PropLine {
-    type Err = PropParseError;
-
-    fn from_str(txt: &str) -> Result<Self, Self::Err> {
-        if txt.starts_with("#") {
-            return Ok(PropLine::Comment);
-        }
-        let bits: Vec<&str> = txt.splitn(2, '=').collect();
-        if bits.len() != 2 {
-            return Err(PropParseError::NoValue);
-        }
-        return Ok(PropLine::Prop(bits[0].to_string(), bits[1].to_string()))
-    }
-}
-
-fn read_properties(path: &Path) -> io::Result<(u16, String)>  {
-    let mut port: u16 = 0;
-    let mut auth: String = "".to_string();
-    for line in io::BufReader::new(File::open(path)?).lines().map(
-        |l| -> io::Result<PropLine> { match l?.parse::<PropLine>() {
-        Ok(pl) => Ok(pl),
-        Err(err) => Err(err.into()),
-    }}) {
-        match line? {
-            PropLine::Comment => {},
-            PropLine::Prop(k, v) => match k.as_ref() {
-                "rcon.port" => {
-                    if let Ok(num) = v.parse() {
-                        port = num;
-                    }
-                },
-                "rcon.password" => {
-                    auth = v;
-                }
-                &_ => {}
-            }
-        }
-    }
-    Ok((port, auth))
-}
-
-
-fn find_serverprops() -> Option<PathBuf> {
-    // 1. Environment variable MINECRAFT_ROOT
-    for (key, value) in env::vars() {
-        if key == "MINECRAFT_ROOT" {
-            return Some(PathBuf::from(value).join("server.properties"));
-        }
-    }
-    // 2. The container's predefined root
-    let container_root = Path::new("/mc"); // TODO: Can we read this from a compile flag?
-    if container_root.exists() {
-        return Some(container_root.join("server.properties"));
-    }
-    // Can't find it
-    None
+fn read_portauth(path: &Path) -> Result<(u16, String), Error>  {
+    let props = read_properties(path)?;
+    Ok((
+        props.get("rcon.port").unwrap_or(&String::from("0")).parse()?,
+        props.get("rcon.password").unwrap_or(&String::from("")).to_string()
+    ))
 }
